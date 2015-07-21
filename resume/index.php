bd164c43fb831a0c64df22c5b36b9e960dbdc215
Jonathan Felchlin <jonathan@xgecko.com>
2015-07-20 17:23:05 -0700

    Added markdown support in /resume

--- resume/index.php
+++ resume/index.php
@@ -5,57 +5,60 @@
 
     use Symfony\Component\Yaml\Parser;
 
-    $data_dir = dirname(__FILE__).'/data/';
+    $data_dir = dirname(__FILE__) . '/data/';
+    $template_dir = dirname(__FILE__) . '/templates';
 
     $phone_match = '~.*(\d{3})[^\d]{,7}(\d{3})[^\d]{,7}(\d{4}).*~';
     $phone_replace = '($1) $2-$3';
 
-    $template_suffix = $_GET[ 'format' ] && $_GET[ 'format' ] != 'htm' ? '.' . $_GET[ 'format' ] : '';
+    $template_suffix = $_GET[ 'format' ] ? '.' . $_GET[ 'format' ] : '';
 
-    $mustache = new Mustache_Engine(array(
+    $template_loader_options = $options =  array(
+        'extension' => '.mustache'
+    );
+
+    // handle unsupported formats
+    if ( !file_exists( $template_dir . '/resume' . $template_suffix . $template_loader_options[ 'extension' ] ) ) {
+        $template_suffix = '';
+    }
+
+    $mustache = new Mustache_Engine( array(
         'template_class_prefix' => '__MyTemplates_',
-        'cache' => dirname(__FILE__).'/tmp/cache/mustache',
+        'cache' => dirname( __FILE__ ) . '/tmp/cache/mustache',
         // 'cache_file_mode' => 0666, // Please, configure your umask instead of doing this :)
         // 'cache_lambda_templates' => true,
-        'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/templates'),
-        'partials_loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/templates'),
+        'loader' => new Mustache_Loader_FilesystemLoader( $template_dir, $template_loader_options ),
+        'partials_loader' => new Mustache_Loader_FilesystemLoader( $template_dir, $template_loader_options ),
         // 'helpers' => array('i18n' => function($text) {
         //     // do something translatey here...
         // }),
-        'escape' => function($value) {
-            return htmlspecialchars($value, ENT_COMPAT, 'UTF-8');
+        'escape' => function( $value ) {
+            return htmlspecialchars( $value, ENT_COMPAT, 'UTF-8' );
         },
         'charset' => 'ISO-8859-1',
-        'logger' => new Mustache_Logger_StreamLogger('php://stderr'),
+        'logger' => new Mustache_Logger_StreamLogger( 'php://stderr' ),
         'strict_callables' => true,
-        'pragmas' => [Mustache_Engine::PRAGMA_FILTERS],
-    ));
+        'pragmas' => [ Mustache_Engine::PRAGMA_FILTERS ],
+    ) );
 
-    // $file_contents = file_get_contents($data_dir.'jonathan_felchlin.json');
-    // $data = json_decode($file_contents, true);
-
-    $file_contents = file_get_contents($data_dir.'jonathan_felchlin.yaml');
+    $file_contents = file_get_contents( $data_dir.'jonathan_felchlin.yaml' );
 
     $yaml = new Parser();
-    $data = $yaml->parse($file_contents);
+    $data = $yaml->parse( $file_contents );
 
-    // if ($error = json_last_error_msg()) {
-    //     print_r(sprintf("<pre>Encountered the following error: %s\nWhile parsing string:\n'%s'", $error, $file_contents));
-    // }
-
-    if ($data["phone"] != NULL && $data["phone_formatted"] == NULL) {
-        $data["phone_formatted"] = preg_replace($phone_match, $phone_replace, $data["phone"]). "\n";
+    if ( $data[ "phone" ] != NULL && $data[ "phone_formatted" ] == NULL) {
+        $data[ "phone_formatted" ] = preg_replace( $phone_match, $phone_replace, $data["phone"] ). "\n";
     }
 
-    foreach($data["sections"] as &$section) {
-        if ($section["type"] == NULL) {
-            $section["type"] = "section";
+    foreach ( $data[ "sections" ] as &$section ) {
+        if ($section[ "type" ] == NULL) {
+            $section[ "type" ] = "section";
         }
 
-        $partial = $mustache->loadTemplate($section["type"].$template_suffix);
-        $section["rendered_content"] = $partial->render($section);
+        $partial = $mustache->loadTemplate( $section["type"].$template_suffix );
+        $section[ "rendered_content" ] = $partial->render( $section );
     }
 
-    $tpl = $mustache->loadTemplate('resume'.$template_suffix);
-    echo $tpl->render($data);
+    $tpl = $mustache->loadTemplate( 'resume'.$template_suffix );
+    echo $tpl->render( $data );
 ?>
